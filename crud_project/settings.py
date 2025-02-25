90c865f28a73e228be47dce3463cd468d1a4d503
FacundoLescano <facundoleonellescano@gmail.com>
2025-02-25 02:38:28 -0300

    add postgresql and api

--- crud_project/settings.py
+++ crud_project/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+#env = environ.Env()
+#environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -74,13 +77,27 @@ WSGI_APPLICATION = "crud_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.postgresql",
+#        "NAME": "postgres",
+#        "USER": env.str("DJANGO_DB_USER"),
+#        "PASSWORD": env.str("DJANGO_dB_PASSWORD"),
+#        "HOST": env.str("DJANGO_DB_HOST"),
+#        "PORT": "5432",
+#    }
+#}
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "Facundo",
+        "PASSWORD": "velezdelcorazon",
+        "HOST": "db-django-api.cx2ym4y0a85s.us-east-2.rds.amazonaws.com",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Products(models.Model):
-    name = models.TextField(max_length=50)
+    name = models.TextField(max_length=60)
     description = models.TextField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
